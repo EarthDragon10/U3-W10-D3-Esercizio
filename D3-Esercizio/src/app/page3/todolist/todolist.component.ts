da1ffd175338876533d138dcf9f9df9a282e4937
EarthDragon10 <lagudadev@gmail.com>
2022-11-03 09:12:24 +0100

    add ts

--- D3-Esercizio/src/app/page3/todolist/todolist.component.ts
+++ D3-Esercizio/src/app/page3/todolist/todolist.component.ts
@@ -9,6 +9,12 @@ export class TodolistComponent implements OnInit {
   txt_italic: string = '';
   btnClicked: boolean = false;
 
+  todos = [
+    { id: 1, text: 'testo di prova 1' },
+    { id: 2, text: 'testo di prova 2' },
+    { id: 3, text: 'testo di prova 3' },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
